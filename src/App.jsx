21f1266a365e443c3530329dc91d12fabe385cea
Mau8877 <maurogupri@gmail.com>
2025-06-28 05:07:53 -0400

    Creacion Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import EstudianteLayout from './layouts/EstudianteLayout';
+import DocenteLayout from './layouts/DocenteLayout';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        {/* Rutas de inicio de sesión */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Rutas de estudiante */}
+        <Route path="/estudiantelayout" element={<EstudianteLayout />}>
+          <Route index element={<DocenteDashboard />} /> {/* Ruta por defecto */}
+        </Route>
+
+        {/* Rutas de docente */}
+        <Route path="/docentelayout" element={<DocenteLayout />}>
+          <Route index element={<DocenteDashboard />} /> {/* Ruta por defecto */}
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
